f5eeb91d90bd1aac048b04fb704ebe50d181c92c
ajoa00liveira <a26001@alunos.ipca.pt>
2024-05-18 19:08:23 +0100

    Angular setup

---
+++ museu-virtual/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { RegisterComponent } from './views/register/register.component';
+import { LoginComponent } from './views/login/login.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    { path: '**', component: PageNotFoundComponent }
+];
