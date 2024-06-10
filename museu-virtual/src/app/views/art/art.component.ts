c91d1f5ec322ba1607a6334b41fc5129159d5abd
ajoa00liveira <a26001@alunos.ipca.pt>
2024-06-10 03:12:03 +0100

    img trade working

--- museu-virtual/src/app/views/art/art.component.ts
+++ museu-virtual/src/app/views/art/art.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-art',
   standalone: true,
@@ -10,7 +9,12 @@ import { Component } from '@angular/core';
 })
 
 export class ArtComponent {
+  bigFrameImage : string = "../../../assets/images/art/art-display1.jpg"; //default big frame image
 
+  // Método chamado quando uma imagem em small-frames é clicada
+  updateBigFrameImage(imagePath: string): void {
+    this.bigFrameImage = imagePath; // Atualiza o caminho da imagem do big-frame com o caminho da imagem clicada
+  }
 }
 
 
